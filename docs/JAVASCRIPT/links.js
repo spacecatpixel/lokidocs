7e002ee48605a44f691f2b5f898fab548638233f
Otis <johnathan@coinstop.com.au>
2019-01-15 16:56:52 +1100

    Added Menu bar to Lokidocs

---
+++ docs/JAVASCRIPT/links.js
@@ -0,0 +1,17 @@
+
+/* Gives Class:Home an ID */
+var favicon = document.getElementsByClassName("md-logo")[0];
+favicon.id="Home";
+
+/* Adds link to ID Home */
+var faviconlink = document.getElementById('Home');
+faviconlink.href = 'https://www.loki.network';
+
+/* Removes Loki Docs Title text and replaces with Menu Bar*/
+var headers = document.getElementsByClassName("md-header-nav__topic");
+for (var i = headers.length - 1; i >= 0; i--) {
+	if (headers[i].innerText === 'Loki Docs') {
+		headers[i].innerHTML = '<a href="https://www.loki.network">Home </a><a href="../..">Documentation </a><a href="https://loki.network/#team-section">Team </a><a href="https://loki.network/#papers">Papers </a> <a href="https://loki.network/blog/"">Blog</a>';
+	}
+}
+
