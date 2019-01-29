3099dbf644114936978ef1a3a8d627eb7cc19375
Otis <johnathan@coinstop.com.au>
2019-01-29 16:42:50 +1100

    Force menu bar to remain when scrolling.

--- docs/JAVASCRIPT/style.js
+++ docs/JAVASCRIPT/style.js
@@ -1,9 +1,3 @@
 window.onscroll = function(e) {
-    const title = document.getElementsByClassName("md-header-nav__title")[0];
-    if (this.oldScroll > this.scrollY) {
-        title.setAttribute("data-md-state", "");
-    } else if (window.pageYOffset >= 200) {
-        title.setAttribute("data-md-state", "active");
-    }
-    this.oldScroll = this.scrollY;
+      document.getElementsByClassName("md-header-nav__title")[0].setAttribute("data-md-state", "");
 }
